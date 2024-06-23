d2cf739ff5fc048b9df0ecbb7bbb3dfa6b11b7fb
Gabe Cook <gabe565@gmail.com>
2024-06-23 04:16:55 -0500

    perf(kemono): Add creator cache

--- internal/kemono/post_handler.go
+++ internal/kemono/post_handler.go
@@ -7,11 +7,13 @@ import (
 	"html/template"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gabe565/transsmute/internal/feed"
 	"github.com/gabe565/transsmute/internal/templatefuncs"
 	"github.com/gabe565/transsmute/internal/util"
 	"github.com/go-chi/chi/v5"
+	"github.com/jellydator/ttlcache/v3"
 )
 
 //go:embed post.html.gotmpl
@@ -23,22 +25,44 @@ var postTmpl = template.Must(
 )
 
 func postHandler(host string) http.HandlerFunc {
+	type creatorCacheKey struct {
+		service string
+		creator string
+	}
+
+	creatorCache := ttlcache.New[creatorCacheKey, *Creator](
+		ttlcache.WithTTL[creatorCacheKey, *Creator](24*time.Hour),
+		ttlcache.WithDisableTouchOnHit[creatorCacheKey, *Creator](),
+	)
+	go creatorCache.Start()
+
 	return func(w http.ResponseWriter, r *http.Request) {
-		creator, err := GetCreatorInfo(r.Context(), host, chi.URLParam(r, "creator"), chi.URLParam(r, "service"))
-		if err != nil {
-			if errors.Is(err, ErrCreatorNotFound) {
-				http.Error(w, err.Error(), http.StatusNotFound)
-				return
-			} else if errors.Is(err, util.ErrUpstreamRequest) {
-				http.Error(w, err.Error(), http.StatusServiceUnavailable)
-				return
+		cacheKey := creatorCacheKey{
+			service: chi.URLParam(r, "service"),
+			creator: chi.URLParam(r, "creator"),
+		}
+		var creator *Creator
+		if cached := creatorCache.Get(cacheKey); cached != nil {
+			creator = cached.Value()
+		} else {
+			var err error
+			if creator, err = GetCreatorInfo(r.Context(), host, cacheKey.creator, cacheKey.service); err != nil {
+				if errors.Is(err, ErrCreatorNotFound) {
+					http.Error(w, err.Error(), http.StatusNotFound)
+					return
+				} else if errors.Is(err, util.ErrUpstreamRequest) {
+					http.Error(w, err.Error(), http.StatusServiceUnavailable)
+					return
+				}
+				panic(err)
 			}
-			panic(err)
+			creatorCache.Set(cacheKey, creator, ttlcache.DefaultTTL)
 		}
 
 		pagesRaw := r.URL.Query().Get("pages")
 		pages := uint64(1)
 		if pagesRaw != "" {
+			var err error
 			if pages, err = strconv.ParseUint(pagesRaw, 10, 64); err != nil || pages == 0 {
 				http.Error(w, "pages must be a positive integer", http.StatusBadRequest)
 				return
