4705e1b0441e6af6f32546a29b71ea6a5211c8f2
Gabe Cook <gabe565@gmail.com>
2024-08-07 16:59:47 -0500

    chore(cmd): Clean up versioning logic

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -12,24 +12,22 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var version = "beta"
-
-func New() *cobra.Command {
-	version, commit := buildVersion(version)
-
+func New(opts ...Option) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:  "transsmute",
 		Long: "Build RSS feeds for websites that don't provide them.",
 		RunE: run,
-
-		Version:     version,
-		Annotations: map[string]string{"commit": commit},
 
 		DisableAutoGenTag: true,
 	}
 	conf := config.New()
 	conf.RegisterFlags(cmd)
 	cmd.SetContext(config.NewContext(context.Background(), conf))
+
+	for _, opt := range opts {
+		opt(cmd)
+	}
+
 	return cmd
 }
 
@@ -43,7 +41,7 @@ func run(cmd *cobra.Command, _ []string) error {
 		return err
 	}
 
-	slog.Info("Transsmute", "version", version, "commit", cmd.Annotations["commit"])
+	slog.Info("Transsmute", "version", cmd.Annotations["version"], "commit", cmd.Annotations["commit"])
 
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
 	defer cancel()
--- main.go
+++ main.go
@@ -6,8 +6,11 @@ import (
 	"github.com/gabe565/transsmute/cmd"
 )
 
+var version = "beta"
+
 func main() {
-	if err := cmd.New().Execute(); err != nil {
+	root := cmd.New(cmd.WithVersion(version))
+	if err := root.Execute(); err != nil {
 		os.Exit(1)
 	}
 }
