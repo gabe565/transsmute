106cdd7f8f30d6cea46fabd51491bdd000c406cb
Gabe Cook <gabe565@gmail.com>
2025-03-02 17:11:57 -0600

    chore(youtube): Simplify HTML template functions

--- internal/youtube/html/html.go
+++ internal/youtube/html/html.go
@@ -3,7 +3,6 @@ package html
 import (
 	"html/template"
 	"net/url"
-	"path"
 	"regexp"
 	"strconv"
 	"strings"
@@ -13,60 +12,15 @@ import (
 var hashtagRe = regexp.MustCompile(`(^|\s)#([A-Za-z0-9_]+)`)
 
 func FormatHashtags(s string) string {
-	matches := hashtagRe.FindAllStringSubmatch(s, -1)
-	if len(matches) == 0 {
-		return s
-	}
-
-	var buf strings.Builder
-	buf.Grow(len(s))
-	var offset int
-	for _, match := range matches {
-		v := match[0]
-		prefix := match[1]
-		slug := match[2]
-
-		idx := strings.Index(s[offset:], v)
-		if idx == -1 {
-			continue
-		}
-		buf.WriteString(s[offset : offset+idx])
-		offset += idx + len(v)
-
-		u := url.URL{
-			Scheme: "https",
-			Host:   "youtube.com",
-			Path:   path.Join("hashtag", slug),
-		}
-		v = template.HTMLEscapeString(prefix) + `<a href="` + u.String() + `">#` + template.HTMLEscapeString(slug) + `</a>`
-		buf.WriteString(v)
-	}
-	buf.WriteString(s[offset:])
-
-	return buf.String()
+	return hashtagRe.ReplaceAllString(s, `$1<a href="https://youtube.com/hashtag/$2">#$2</a>`)
 }
 
 var timestampRe = regexp.MustCompile("([0-9]:)?[0-9]+:[0-9]+")
 
 func FormatTimestamps(id, s string) string {
-	times := timestampRe.FindAllString(s, -1)
-	if len(times) == 0 {
-		return s
-	}
-
-	var buf strings.Builder
-	buf.Grow(len(s))
-	var offset int
-	for _, v := range times {
-		idx := strings.Index(s[offset:], v)
-		if idx == -1 {
-			continue
-		}
-		buf.WriteString(s[offset : offset+idx])
-		offset += idx + len(v)
-
-		cleaned := v
-		if strings.Count(v, ":") == 2 {
+	return timestampRe.ReplaceAllStringFunc(s, func(s string) string {
+		cleaned := s
+		if strings.Count(s, ":") == 2 {
 			cleaned = strings.Replace(cleaned, ":", "h", 1)
 		}
 		cleaned = strings.Replace(cleaned, ":", "m", 1)
@@ -83,12 +37,8 @@ func FormatTimestamps(id, s string) string {
 				}.Encode(),
 			}
 
-			v = `<a href="` + u.String() + `">` + template.HTMLEscapeString(v) + `</a>`
+			s = `<a href="` + u.String() + `">` + template.HTMLEscapeString(s) + `</a>`
 		}
-
-		buf.WriteString(v)
-	}
-	buf.WriteString(s[offset:])
-
-	return buf.String()
+		return s
+	})
 }
