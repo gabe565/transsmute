9c5d8df732821828cbce575563a6ecb1c5b60f64
Gabe Cook <gabe565@gmail.com>
2025-08-12 17:35:55 -0500

    chore(config): rename flag consts

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -3,57 +3,57 @@ package config
 import "github.com/spf13/cobra"
 
 const (
-	ListenAddressFlag = "address"
+	FlagListenAddress = "address"
 
-	DockerEnabledFlag      = "docker-enabled"
-	GHCRUsernameFlag       = "ghcr-username"
-	GHCRPasswordFlag       = "ghcr-password"
-	GHCRAppIDFlag          = "ghcr-app-id"
-	GHCRInstallationIDFlag = "ghcr-installation-id"
-	GHCRPrivateKeyFlag     = "ghcr-private-key"
-	GHCRPrivateKeyPathFlag = "ghcr-private-key-path"
-	DockerHubUsernameFlag  = "dockerhub-username"
-	DockerHubPasswordFlag  = "dockerhub-password" //nolint:gosec
+	FlagDockerEnabled      = "docker-enabled"
+	FlagGHCRUsername       = "ghcr-username"
+	FlagGHCRPassword       = "ghcr-password"
+	FlagGHCRAppID          = "ghcr-app-id"
+	FlagGHCRInstallationID = "ghcr-installation-id"
+	FlagGHCRPrivateKey     = "ghcr-private-key"
+	FlagGHCRPrivateKeyPath = "ghcr-private-key-path"
+	FlagDockerHubUsername  = "dockerhub-username"
+	FlagDockerHubPassword  = "dockerhub-password" //nolint:gosec
 
-	YouTubeEnabledFlag = "youtube-enabled"
-	YouTubeAPIKeyFlag  = "youtube-key"
+	FlagYouTubeEnabled = "youtube-enabled"
+	FlagYouTubeAPIKey  = "youtube-key"
 
-	KemonoEnabledFlag = "kemono-enabled"
-	KemonoHostsFlag   = "kemono-hosts"
+	FlagKemonoEnabled = "kemono-enabled"
+	FlagKemonoHosts   = "kemono-hosts"
 )
 
 func (c *Config) RegisterFlags(cmd *cobra.Command) {
-	cmd.Flags().StringVar(&c.ListenAddress, ListenAddressFlag, c.ListenAddress, "Listen address")
+	cmd.Flags().StringVar(&c.ListenAddress, FlagListenAddress, c.ListenAddress, "Listen address")
 
-	cmd.Flags().BoolVar(&c.Docker.Enabled, DockerEnabledFlag, c.Docker.Enabled, "Docker API enabled")
-	cmd.Flags().StringVar(&c.Docker.GHCR.Username, GHCRUsernameFlag, c.Docker.GHCR.Username,
+	cmd.Flags().BoolVar(&c.Docker.Enabled, FlagDockerEnabled, c.Docker.Enabled, "Docker API enabled")
+	cmd.Flags().StringVar(&c.Docker.GHCR.Username, FlagGHCRUsername, c.Docker.GHCR.Username,
 		"GitHub username for ghcr.io",
 	)
-	cmd.Flags().StringVar(&c.Docker.GHCR.Password, GHCRPasswordFlag, c.Docker.GHCR.Password,
+	cmd.Flags().StringVar(&c.Docker.GHCR.Password, FlagGHCRPassword, c.Docker.GHCR.Password,
 		"GitHub personal access token for ghcr.io",
 	)
-	cmd.Flags().Int64Var(&c.Docker.GHCR.AppID, GHCRAppIDFlag, c.Docker.GHCR.AppID, "GitHub app ID")
-	cmd.Flags().Int64Var(&c.Docker.GHCR.InstallationID, GHCRInstallationIDFlag, c.Docker.GHCR.InstallationID,
+	cmd.Flags().Int64Var(&c.Docker.GHCR.AppID, FlagGHCRAppID, c.Docker.GHCR.AppID, "GitHub app ID")
+	cmd.Flags().Int64Var(&c.Docker.GHCR.InstallationID, FlagGHCRInstallationID, c.Docker.GHCR.InstallationID,
 		"GitHub installation ID",
 	)
-	cmd.Flags().StringVar(&c.Docker.GHCR.PrivateKey, GHCRPrivateKeyFlag, c.Docker.GHCR.PrivateKey,
+	cmd.Flags().StringVar(&c.Docker.GHCR.PrivateKey, FlagGHCRPrivateKey, c.Docker.GHCR.PrivateKey,
 		"GitHub app private key",
 	)
-	cmd.Flags().StringVar(&c.Docker.GHCR.PrivateKeyPath, GHCRPrivateKeyPathFlag, c.Docker.GHCR.PrivateKeyPath,
+	cmd.Flags().StringVar(&c.Docker.GHCR.PrivateKeyPath, FlagGHCRPrivateKeyPath, c.Docker.GHCR.PrivateKeyPath,
 		"GitHub app private key file path",
 	)
-	cmd.Flags().StringVar(&c.Docker.DockerHub.Username, DockerHubUsernameFlag, c.Docker.DockerHub.Username,
+	cmd.Flags().StringVar(&c.Docker.DockerHub.Username, FlagDockerHubUsername, c.Docker.DockerHub.Username,
 		"DockerHub username for private repos",
 	)
-	cmd.Flags().StringVar(&c.Docker.DockerHub.Password, DockerHubPasswordFlag, c.Docker.DockerHub.Password,
+	cmd.Flags().StringVar(&c.Docker.DockerHub.Password, FlagDockerHubPassword, c.Docker.DockerHub.Password,
 		"DockerHub password for private repos",
 	)
 
-	cmd.Flags().BoolVar(&c.YouTube.Enabled, YouTubeEnabledFlag, c.YouTube.Enabled, "YouTube API enabled")
-	cmd.Flags().StringVar(&c.YouTube.APIKey, YouTubeAPIKeyFlag, c.YouTube.APIKey, "YouTube API key")
+	cmd.Flags().BoolVar(&c.YouTube.Enabled, FlagYouTubeEnabled, c.YouTube.Enabled, "YouTube API enabled")
+	cmd.Flags().StringVar(&c.YouTube.APIKey, FlagYouTubeAPIKey, c.YouTube.APIKey, "YouTube API key")
 
-	cmd.Flags().BoolVar(&c.Kemono.Enabled, KemonoEnabledFlag, c.Kemono.Enabled, "Kemono API enabled")
-	cmd.Flags().StringToStringVar(&c.Kemono.Hosts, KemonoHostsFlag, c.Kemono.Hosts,
+	cmd.Flags().BoolVar(&c.Kemono.Enabled, FlagKemonoEnabled, c.Kemono.Enabled, "Kemono API enabled")
+	cmd.Flags().StringToStringVar(&c.Kemono.Hosts, FlagKemonoHosts, c.Kemono.Hosts,
 		"Kemono API hosts, where the key is the URL prefix and the value is the host",
 	)
 }
